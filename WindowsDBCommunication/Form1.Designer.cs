f97747a140bc0d7d5fb51c7951ef04cea523be52
mythr <mythr@LAPTOP-78LDJ37C>
2024-09-19 19:02:03 -0700

    Add project files.

---
+++ WindowsDBCommunication/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsDBCommunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmpid = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtEmpSal = new System.Windows.Forms.TextBox();
+            this.txtEmpAdd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmpid
+            // 
+            this.txtEmpid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpid.Location = new System.Drawing.Point(468, 110);
+            this.txtEmpid.Name = "txtEmpid";
+            this.txtEmpid.Size = new System.Drawing.Size(129, 30);
+            this.txtEmpid.TabIndex = 0;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpName.Location = new System.Drawing.Point(468, 209);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(129, 30);
+            this.txtEmpName.TabIndex = 1;
+            // 
+            // txtEmpSal
+            // 
+            this.txtEmpSal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpSal.Location = new System.Drawing.Point(468, 319);
+            this.txtEmpSal.Name = "txtEmpSal";
+            this.txtEmpSal.Size = new System.Drawing.Size(129, 30);
+            this.txtEmpSal.TabIndex = 2;
+            // 
+            // txtEmpAdd
+            // 
+            this.txtEmpAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpAdd.Location = new System.Drawing.Point(468, 421);
+            this.txtEmpAdd.Name = "txtEmpAdd";
+            this.txtEmpAdd.Size = new System.Drawing.Size(129, 30);
+            this.txtEmpAdd.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EmployeeId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(138, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "EmployeeName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "EmployeeSal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(141, 427);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "EmployeeAddress";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(144, 543);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(128, 64);
+            this.btnInsert.TabIndex = 8;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(386, 543);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(128, 64);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(622, 543);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(128, 64);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 702);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmpAdd);
+            this.Controls.Add(this.txtEmpSal);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtEmpid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmpid;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtEmpSal;
+        private System.Windows.Forms.TextBox txtEmpAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ WindowsDBCommunication/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; // step-1
+
+namespace WindowsDBCommunication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            //Logic to communicate with the DB and insert into the table
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-78LDJ37C\\SQLEXPRESS;Initial Catalog=Sample;Integrated Security=True;TrustServerCertificate=True");  //step-2
+
+            SqlCommand cmd = new SqlCommand("Insert into Employee (EmpId,EmpName,EmpSal,EmpAdd)Values(@empid,@empname,@empsal,@empadd)", con);// step-3
+
+            cmd.Parameters.AddWithValue("@empid", txtEmpid.Text);
+            cmd.Parameters.AddWithValue("@empname", txtEmpName.Text);
+            cmd.Parameters.AddWithValue("@empsal", txtEmpSal.Text);
+            cmd.Parameters.AddWithValue("@empadd", txtEmpAdd.Text);
+
+            con.Open();  //step-4
+
+            cmd.ExecuteNonQuery();  // step-5 // insert,update,delete  --action commands 
+            con.Close();
+            MessageBox.Show("Record Inserted Sucessfully");
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //UpdateLogics
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-78LDJ37C\\SQLEXPRESS;Initial Catalog=Sample;Integrated Security=True;TrustServerCertificate=True");
+            SqlCommand cmd = new SqlCommand("Update Employee set EmpName=@empname,EmpSal=@empsal,EmpAdd=@empadd where EmpId=@empid", con);
+            cmd.Parameters.AddWithValue("@empid", txtEmpid.Text);
+            cmd.Parameters.AddWithValue("@empname", txtEmpName.Text);
+            cmd.Parameters.AddWithValue("@empsal", txtEmpSal.Text);
+            cmd.Parameters.AddWithValue("@empadd", txtEmpAdd.Text);
+            con.Open();  //step-4
+
+            cmd.ExecuteNonQuery();  // step-5 // insert,update,delete  --action commands 
+            con.Close();
+            MessageBox.Show("Record UPDATED Sucessfully");
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Delete logic 
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-78LDJ37C\\SQLEXPRESS;Initial Catalog=Sample;Integrated Security=True;TrustServerCertificate=True");
+            SqlCommand cmd = new SqlCommand("Delete Employee where EmpId=@empid", con);
+            cmd.Parameters.AddWithValue("@empid", txtEmpid.Text);
+            
+            con.Open();  //step-4
+
+            cmd.ExecuteNonQuery();  // step-5 // insert,update,delete  --action commands 
+            con.Close();
+            MessageBox.Show("Record deleted Sucessfully");
+
+        }
+    }
+}
